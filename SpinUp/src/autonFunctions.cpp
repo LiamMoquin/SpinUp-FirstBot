a18245bf5c0c52c3c91b82452b9c8696d3b7f3d5
LiamMoquin <84659300+LiamMoquin@users.noreply.github.com>
2022-09-20 16:18:52 -0700

    idk I just got back from summer and need to commit this

--- SpinUp/src/autonFunctions.cpp
+++ SpinUp/src/autonFunctions.cpp
@@ -27,7 +27,17 @@ void visionStrafe(float speed /*speed to move at*/) //strafes to track an object
     float deadzone = .002; //sets deadzone (acceptable error range)
 
     Vision20.takeSnapshot(Vision20__SIG_1); //takes a snapshot of objects in signature (sig) 1
-
+    //spin in a circle to the left
+    while(Vision20.objects[0].exists == false) //while nothing is in the sensor fov
+    {
+      Vision20.takeSnapshot(Vision20__SIG_1);
+      frontLeft.setVelocity(-speed,percent);
+      rearLeft.setVelocity(-speed,percent);
+      frontRight.setVelocity(speed,percent);
+      rearRight.setVelocity(speed,percent);
+      allspin();
+    }
+    
     while(Vision20.objects[0].exists) //while an object matching sig 1 exists
     {
       Vision20.takeSnapshot(Vision20__SIG_1);
@@ -40,7 +50,6 @@ void visionStrafe(float speed /*speed to move at*/) //strafes to track an object
         rearLeft.setVelocity(speed,percent);
         frontRight.setVelocity(speed,percent);
         rearRight.setVelocity(-speed,percent);
-        allspin();
       }
       //strafe right
       while(Vision20.objects[0].centerX >= (157 + deadzone)) //while the object's center is to the right of the sensor center
@@ -57,16 +66,6 @@ void visionStrafe(float speed /*speed to move at*/) //strafes to track an object
         Vision20.takeSnapshot(Vision20__SIG_1);
         allstop();
       }
-    }
-    //spin in a circle tot the left
-    while(Vision20.objects[0].exists == false) //while nothing is in the sensor fov
-    {
-      Vision20.takeSnapshot(Vision20__SIG_1);
-      frontLeft.setVelocity(-speed,percent);
-      rearLeft.setVelocity(-speed,percent);
-      frontRight.setVelocity(speed,percent);
-      rearRight.setVelocity(speed,percent);
-      allspin();
     }
   }
 }
