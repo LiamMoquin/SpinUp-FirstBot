f630beee50e8835098d9aad30af3edd1726961fa
LiamMoquin <84659300+LiamMoquin@users.noreply.github.com>
2022-10-14 16:46:45 -0700

    Update PIDs.cpp

--- SpinUp/src/PIDs.cpp
+++ SpinUp/src/PIDs.cpp
@@ -12,6 +12,11 @@ void mtrReset(){
   rearRight.resetRotation();
 }
 
+double mtrAvg()
+{
+  return ((frontLeft.rotation(degrees) + frontRight.rotation(degrees) + rearLeft.rotation(degrees) + rearRight.rotation(degrees)) / 4);
+}
+
 double totalTargetDeg; //degrees to rotate over the whole motion
 double lastError; //error in the last iteration
 double error; //error in the current iteration
@@ -21,7 +26,6 @@ double akD = 0; //derivative tuning value
 void driveStraighti(float dist, float vi = 0, float vf = 100, float rUpDist = 0.2){
   mtrReset();
 
-  double mtrAvg = (frontLeft.rotation(degrees) + frontRight.rotation(degrees) + rearLeft.rotation(degrees) + rearRight.rotation(degrees)) / 4;
   error = 0; //sets error to 0
   lastError = 0; //sets lastError to 0
 
@@ -31,8 +35,17 @@ void driveStraighti(float dist, float vi = 0, float vf = 100, float rUpDist = 0.
 
   double targetDeg = (totalTargetDeg * rUpDist);
 
-  while (mtrAvg <= targetDeg)
+  while (mtrAvg() <= targetDeg)
   {
-    error = abs()
+    error = (abs(frontLeft.rotation(degrees) + rearLeft.rotation(degrees)) - (abs(frontRight.rotation(degrees) + rearRight.rotation(degrees))));
+    double accelCalc = (vf - vi) * (mtrAvg() / targetDeg) + vi;
+
+    frontLeft.setVelocity((((error * akP) + akD * (error - lastError)) + accelCalc), rpm);
+    rearRight.setVelocity((((error * akP) + akD * (error - lastError)) + accelCalc), rpm);
+    frontRight.setVelocity((accelCalc - ((error * akP) + akD * (error - lastError))), rpm);
+    rearRight.setVelocity((accelCalc - ((error * akP) + akD * (error - lastError))), rpm);
+    
+    lastError = error;
+    wait(25, msec);
   }
 }
